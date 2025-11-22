549b3e33dba3ea8e3f60ed9b43bd893f790b6e6a
Lizzy <martinezdelgadoelisabeth@gmail.com>
2025-11-22 12:26:48 +0100

    base infrastructure

---
+++ backend/src/test/java/com/ifco/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifco.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
